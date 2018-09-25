4e1223de156ee0c3482231c0091ceef3c82d563a
E-Rooijackers <eduard.rooijackers@live.nl>
2018-09-25 15:58:53 +0200

    Modified Parser..

--- Parser.java
+++ Parser.java
@@ -6,17 +6,86 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.*;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 class Parser
 {
-    public Document getXmlDoc(List<Album> albums)
+    public Document getXML(List<Album> albums)
     {
-        return albums;
+    	try {
+            DocumentBuilderFactory dbFactory =
+            DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+            
+            // root element
+            Element rootElement = doc.createElement("albums");
+            doc.appendChild(rootElement);
+            
+            for(int i = 0; i < albums.size(); i++)
+            {
+            	Element album = doc.createElement("album");
+            	
+            	Element name = doc.createElement("name");
+            	name.setNodeValue(albums.get(i).name);
+            	album.appendChild(name);
+            	
+            	Element artist = doc.createElement("artist");
+            	name.setNodeValue(albums.get(i).artist);
+            	album.appendChild(artist);
+            	
+            	Element genre = doc.createElement("genre");
+            	name.setNodeValue(albums.get(i).genre);
+            	album.appendChild(genre);
+            	
+            	Element year = doc.createElement("year");
+            	year.setNodeValue("" + albums.get(i).year);
+            	album.appendChild(year);
+            	
+            }
+            
+            
+            
+            rootElement.appendChild(album);
+
+            // setting attribute to element
+            Attr attr = doc.createAttribute("company");
+            attr.setValue("Ferrari");
+            supercar.setAttributeNode(attr);
+
+            // carname element
+            Element carname = doc.createElement("carname");
+            Attr attrType = doc.createAttribute("type");
+            attrType.setValue("formula one");
+            carname.setAttributeNode(attrType);
+            carname.appendChild(doc.createTextNode("Ferrari 101"));
+            supercar.appendChild(carname);
+
+            Element carname1 = doc.createElement("carname");
+            Attr attrType1 = doc.createAttribute("type");
+            attrType1.setValue("sports");
+            carname1.setAttributeNode(attrType1);
+            carname1.appendChild(doc.createTextNode("Ferrari 202"));
+            supercar.appendChild(carname1);
+
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("C:\\cars.xml"));
+            transformer.transform(source, result);
+            
+            // Output to console for testing
+            StreamResult consoleResult = new StreamResult(System.out);
+            transformer.transform(source, consoleResult);
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
     }
     
-    public List<Album> getXmlData(Document doc)
+    public List<Album> getData(Document doc)
     {
         List<Album> albums = new ArrayList<Album>();
         
@@ -48,7 +117,7 @@ class Parser
             				album.genre = nlAlbum.item(n).getNodeValue();
             				break;
             			case "year":
-            				album.year = nlAlbum.item(n).getNodeValue();
+            				album.year = Integer.parseInt(nlAlbum.item(n).getNodeValue());
             				break;
             			default:
             				System.out.println("XML document not as expected in Parser.getXmlData()");
