ff9e1ddf5839f18ba1864ffb7af7b838254246aa
Merlinidrini <merlijn.d.lange@hotmail.com>
2018-09-27 12:07:32 +0200

    added cdcollection.sql

--- cdcollection.sql
+++ cdcollection.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2018 at 10:10 AM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Gegenereerd op: 27 sep 2018 om 12:02
+-- Serverversie: 10.1.33-MariaDB
+-- PHP-versie: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,36 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `albums`
+-- Tabelstructuur voor tabel `albums`
 --
 
-CREATE TABLE `albums` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `artist` varchar(300) NOT NULL,
-  `genre` int(100) NOT NULL,
-  `year_of_release` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `genre` varchar(100) NOT NULL,
+  `year` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Gegevens worden geëxporteerd voor tabel `albums`
 --
 
---
--- Indexes for table `albums`
---
-ALTER TABLE `albums`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `albums`
---
-ALTER TABLE `albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+INSERT INTO `albums` (`id`, `name`, `artist`, `genre`, `year`) VALUES
+(1, 'HOI', 'HOI', '1', 2005),
+(2, 'jo', 'hoi', 'dag', 1991),
+(3, 'hai', 'hai', 'hai', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
